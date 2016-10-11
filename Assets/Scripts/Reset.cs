67d2499dd4d6f26ea69d562141a432196e4c0e6b
22lrfuller <lfuller10@outlook.com>
2016-10-11 19:45:04 -0400

    init commit

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reset : MonoBehaviour {
+
+	void Start () {
+
+	}
+		
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+			Application.LoadLevel(Application.loadedLevel);
+	}
+}
