8903a8bd5dca3ed9063a7290faa01e2c18c93ffd
22lrfuller <lfuller10@outlook.com>
2016-12-08 20:05:11 -0500

    Added Start Button

---
+++ Assets/Scripts/startMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class startMenuScript : MonoBehaviour {
+
+	public void clickButton (){
+		SceneManager.LoadScene (1);
+	}
+
+	void Start () {
+	
+
+
+	}
+
+	void Update () {
+	
+	}
+}
